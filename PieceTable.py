2ded228fd891cae0eda59462739c4c3d48de9253
vmanvs <manvendravsvats@gmail.com>
2025-09-22 11:11:32 +0530

    Included delete method

--- PieceTable.py
+++ PieceTable.py
@@ -76,7 +76,7 @@ class PieceTable:
         #    ========== ====  (space represents a different buffer)
         #    ^   ^      ^
         #    A   B      C
-        # remember the original buffer is read only, change can only be made in the add buffer
+        # remember the original buffer is read only, changes can only be made in the add buffer
         # A=cur_piece.offset, B=piece_offset, C=added_offset
 
         insert = list(filter(lambda piece: piece.length > 0, insert_pieces))
@@ -90,10 +90,82 @@ class PieceTable:
         :int length: the length of the piece to be deleted
         :return:
         """
-        element = _Piece(True, index, length)
+
+        start_piece_index, start_piece_offset = self.get_piece_and_offset(index)
+        stop_piece_index, stop_piece_offset = self.get_piece_and_offset(index+length)
+        self._text_len -= length
+
+        #Single Piece Logic
+        #if single piece check if delete is at the beginning or end of the piece
+        if start_piece_index == stop_piece_index: #same piece
+            piece = self.pieces[start_piece_index]
+
+            if piece.offset == start_piece_offset: #delete at the beginning
+                piece.offset += length
+                piece.length -= length
+                return
+
+            # ============= (single piece)
+            # ^     ^
+            # A     B
+            # A=piece.offset, B=length, after deletion =>
+            # ******=======
+            #       ^
+            #       C = new offset: piece.offset+length
+
+
+            if (piece.offset + piece.length) == stop_piece_offset: #delete at the end
+                piece.length -= length
+                return
+
+            # ========== (single piece)
+            # ^    ^   ^
+            # A    B   C
+            # A: piece.offset, C: piece.offset+piece.length,  C-B: delete length
+            # After deletion:;
+            # ======**** p.s. The objects are not actually removed just the pointers to them are removed making them inaccessible
+
+        #Multi-Piece Logic
+        start_piece = self.pieces[start_piece_index]
+        end_piece = self.pieces[stop_piece_index]
+
+        delete_pieces = [
+
+            _Piece(start_piece.in_added, start_piece.offset, start_piece_offset-start_piece.offset ),
+            # About piece length :Since we are deleting the entire sequence after start_piece_offset: e.g.
+            # Hello
+            # 01234
+            # lets say we want to delete 'lo'
+            # in this case start_piece_offset = 3 and start_piece.offset = 0
+            # Hel
+            # new_length = 3-0 = 3 which is correct.
+
+            _Piece(end_piece.in_added, stop_piece_offset, end_piece.length + (stop_piece_offset-end_piece.offset)),
+            # About piece offset: since we are deleting everything before the stop_piece_offset we will push the pointer back to that position
+            # Beautiful_World ('_' defines a space character)
+            # 0123456789ABCDE (A=10, B=11, C=12,.....)
+            # lets say we want to delete everything upto World
+            # stop_piece_index = A
+            # end_piece.offset = 0
+            # new offset should be the offset of 'W' i.e. 10
+            # About length: end_piece.length = 15, stop_piece_offset = 10, end_piece.offset = 0
+            # 15 - (10-0) = 5 which is correct
+
+        ]
+
+        delete = list(filter(lambda piece: piece.length > 0, delete_pieces))
+
+        delete_count = stop_piece_index - start_piece_index + 1 #this is for multi piece delete only
+
+        self.pieces = self.replace(start_piece_index, delete_count, delete)
 
 
     def get_piece_and_offset(self, index):
+        """
+        Essentially translates: logical position x in the combined text to; piece:y, position_in_buffer(piece):z
+        :param index:
+        :return:
+        """
 
         if index < 0:
             raise IndexError("Index can't be negative.")
