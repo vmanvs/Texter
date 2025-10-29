cfc16ac0ab7db1f4e27a02bf148a17cdc108ec80
vmanvs <manvendravsvats@gmail.com>
2025-10-30 01:12:58 +0530

    Added some new methods

--- txtarea.py
+++ txtarea.py
@@ -1,12 +1,20 @@
 import os
 
+from textual import events
 from textual.app import App, ComposeResult
+from textual.binding import Binding
 from textual.widgets import TextArea
-from textual.document._document import Document, DocumentBase
+from textual.document._document import DocumentBase
 from pt_for_textarea import PieceTableDocument
 from typing import Optional
 
 class NewTextArea(TextArea):
+    BINDINGS = [
+        Binding("ctrl+s", "save", "Save File", show=True),
+        Binding("ctrl+q", "quit", "Quit", show=True),
+        Binding("ctrl+backspace", "delete_word", "Delete Word"),
+    ]
+
     def __init__(self, **kwargs):
 
         super().__init__(**kwargs)
@@ -19,22 +27,61 @@ class NewTextArea(TextArea):
         except Exception as e:
             return e
 
+    def on_mount(self) -> None:
+        """Fix for rendering glitch where text is offset by one column
+           and the second line is not visible."""
+        self.move_cursor_relative(columns=1)
+        self.delete((0,1), (0,0))
+
+
+    def _on_key(self, event: events.Key) -> None:
+        if event.character == '*':
+            self.insert('**')
+            self.move_cursor_relative(columns=-1)
+            event.prevent_default()
+        if event.character == '(':
+            self.insert('()')
+            self.move_cursor_relative(columns=-1)
+            event.prevent_default()
+        if event.character == '[':
+            self.insert('[]')
+            self.move_cursor_relative(columns=-1)
+            event.prevent_default()
+        if event.character == '_':
+            self.insert('__')
+            self.move_cursor_relative(columns=-1)
+            event.prevent_default()
+        if event.character == '`':
+            self.insert('``')
+            self.move_cursor_relative(columns=-1)
+            event.prevent_default()
+        if event.character == '{':
+            self.insert('{}')
+            self.move_cursor_relative(columns=-1)
+            event.prevent_default()
+
 
 
 class Test(App):
 
     def __init__(self, filename:Optional[str]=None):
         super().__init__()
-        self.filename = filename if filename else "Untitled"
+        self.filename = filename if filename else ""
         self.text = ""
 
         if filename and os.path.exists(filename):
             with open(filename, 'r', encoding='utf-8') as f:
                 self.text = f.read()
 
+    def on_mount(self) -> None:
+        """Creates the editor instance once the document is loaded."""
+        self.editor = self.query_one(NewTextArea)
+
 
     def compose(self) -> ComposeResult:
         yield NewTextArea(text=self.text)
+
+
 
 def main():
     import sys
